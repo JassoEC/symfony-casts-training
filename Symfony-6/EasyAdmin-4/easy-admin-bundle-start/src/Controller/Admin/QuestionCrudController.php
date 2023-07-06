b76344cb95455b9480fba8d250787a546c428fd1
Emanuel Campos <isw.ecamposj@gmail.com>
2023-07-05 22:59:05 -0600

    customize actions

--- Symfony-6/EasyAdmin-4/easy-admin-bundle-start/src/Controller/Admin/QuestionCrudController.php
+++ Symfony-6/EasyAdmin-4/easy-admin-bundle-start/src/Controller/Admin/QuestionCrudController.php
@@ -5,22 +5,33 @@ namespace App\Controller\Admin;
 use App\EasyAdmin\VotesField;
 use App\Entity\Question;
 use App\Entity\User;
+use App\Service\CsvExporter;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 #[IsGranted('ROLE_MODERATOR')]
 class QuestionCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator,
+        private RequestStack $requestStack
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Question::class;
@@ -67,8 +78,6 @@ class QuestionCrudController extends AbstractCrudController
 
         yield AssociationField::new('updatedBy')
             ->onlyOnDetail();
-
-        yield Field::new('isApproved');
     }
 
     public function configureCrud(Crud $crud): Crud
@@ -91,6 +100,30 @@ class QuestionCrudController extends AbstractCrudController
             ->setCssClass('btn btn-success')
             ->setLabel('View on site');
 
+
+        $approveAction = Action::new('approve')
+            ->addCssClass('btn btn-success')
+            ->setIcon('fa fa-check-circle')
+            ->displayAsButton()
+            ->setTemplatePath('admin/approve_action.html.twig')
+            ->linkToCrudAction('approve')
+            ->displayIf(static function (Question $question): bool {
+                return !$question->getIsApproved();
+            });
+
+        $exportAction = Action::new('export')
+            ->linkToUrl(function () {
+                $request = $this->requestStack->getCurrentRequest();
+
+                return $this->adminUrlGenerator
+                    ->setAll($request->query->all())
+                    ->setAction('export')
+                    ->generateUrl();
+            })
+            ->addCssClass('btn btn-success')
+            ->setIcon('fa fa-download')
+            ->createAsGlobalAction();
+
         return parent::configureActions($actions)
 
             ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
@@ -111,7 +144,17 @@ class QuestionCrudController extends AbstractCrudController
             ->setPermission(Action::BATCH_DELETE, 'ROLE_SUPER_ADMIN')
 
             ->add(Crud::PAGE_DETAIL, $viewAction)
-            ->add(Crud::PAGE_INDEX, $viewAction);
+            ->add(Crud::PAGE_INDEX, $viewAction)
+            ->add(Crud::PAGE_DETAIL, $approveAction)
+            ->add(Crud::PAGE_INDEX, $exportAction)
+
+            ->reorder(Crud::PAGE_DETAIL, [
+                'approve',
+                'view',
+                Action::EDIT,
+                Action::INDEX,
+                Action::DELETE
+            ]);
     }
 
     public function configureFilters(Filters $filters): Filters
@@ -146,5 +189,37 @@ class QuestionCrudController extends AbstractCrudController
         }
 
         parent::deleteEntity($entityManager, $entityInstance);
+    }
+
+    function approve(AdminContext $adminContext, EntityManagerInterface $entityManager)
+    {
+
+        $question = $adminContext->getEntity()->getInstance();
+
+        if (!$question instanceof Question) {
+            throw new \LogicException('Entity must be an instance of ' . Question::class);
+        }
+
+        $question->setIsApproved(true);
+        $entityManager->flush();
+
+        $targetUrl = $this->adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Action::DETAIL)
+            ->setEntityId($question->getId())
+            ->generateUrl();
+
+
+        return $this->redirect($targetUrl);
+    }
+
+
+    function export(AdminContext $context, CsvExporter $csvExporter)
+    {
+        $fields = FieldCollection::new($this->configureFields(Crud::PAGE_INDEX));
+        $filters = $this->container->get(FilterFactory::class)->create($context->getCrud()->getFiltersConfig(), $fields, $context->getEntity());
+        $queryBuilder = $this->createIndexQueryBuilder($context->getSearch(), $context->getEntity(), $fields, $filters);
+
+        return $csvExporter->createResponseFromQueryBuilder($queryBuilder, $fields, 'questions.csv');
     }
 }
